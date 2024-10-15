d5f97bbddf1a220d608d0a60632a8b7eb73b5d6f
alireza <es.alrza@gmail.com>
2024-10-15 18:27:24 +0330

    add data, service and web modules for multi-module architecture

--- web/src/main/java/ir/eshghishop/web/EshghishopApplication.java
+++ web/src/main/java/ir/eshghishop/web/EshghishopApplication.java
@@ -1,4 +1,4 @@
-package ir.eshghishop.eshghishop;
+package ir.eshghishop.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
