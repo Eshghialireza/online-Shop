961b5d01da929405bdac31b43bad2ec41d250aaf
alireza <es.alrza@gmail.com>
2024-10-15 16:02:51 +0330

    Initial commit with application.yml and project files

---
+++ src/main/java/ir/eshghishop/eshghishop/EshghishopApplication.java
@@ -0,0 +1,13 @@
+package ir.eshghishop.eshghishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshghishopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshghishopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/eshghishop/eshghishop/EshghishopApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.eshghishop.eshghishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshghishopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
